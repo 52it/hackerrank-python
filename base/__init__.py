ee98ee430e79856718eb387c4e3bedacf0405bd2
Jackie <cyn.cto@gmail.com>
2017-01-05 17:04:42 +0800

    add test function

--- base/__init__.py
+++ base/__init__.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-#--*--utf-8--*--
+#!/usr/bin/env ptyhon
+# -*- coding: utf-8 -*-
 import sys
 
 
@@ -21,6 +21,49 @@ def t3():
             print 'Weird'
         else:
             print 'Not Weird'
+
+
+def t4():
+    a = int(raw_input())
+    b = int(raw_input())
+    print (a+b)
+    print (a-b)
+    print (a*b)
+
+
+def t5():
+    a = int(raw_input())
+    b = int(raw_input())
+    print a // b
+    print a / b
+
+
+def t6():
+    n = int(raw_input())
+    for i in range(0, n):
+        print i * i
+
+
+def t7():
+    leap = False
+    year = int(raw_input())
+    if (year % 4 == 0 and year % 100 > 0) or year % 400 == 0:
+        leap = True
+    print leap
+
+
+def t8():
+    n = int(raw_input())
+    print(*range(1, n+1), sep = '')
+    #  result = 0
+    #  for i in range(1, n+1):
+        #  n -= 1
+        #  if i < 4:
+            #  result += i * pow(10, n)
+        #  elif i == n:
+            #  result
+    #  print result
+
 
 
 if '__main__' == __name__:
