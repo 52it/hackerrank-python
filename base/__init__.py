2024608de2ea2ec18a3abbb6b4e18485f0e367de
Jakcie <cyn.cto@gmail.com>
2017-01-11 14:56:04 +0800

    update learn content

--- base/__init__.py
+++ base/__init__.py
@@ -54,15 +54,43 @@ def t7():
 
 def t8():
     n = int(raw_input())
-    print(*range(1, n+1), sep = '')
-    #  result = 0
-    #  for i in range(1, n+1):
-        #  n -= 1
-        #  if i < 4:
-            #  result += i * pow(10, n)
-        #  elif i == n:
-            #  result
-    #  print result
+    #  print(*range(1, n+1), sep = '')
+    result = 0
+    for i in range(1, n+1):
+        n -= 1
+        if i < 4:
+            result += i * pow(10, n)
+        elif i == n:
+            result
+    print result
+
+
+def t_1_1():
+    mylist = list()
+    for i in xrange(int(raw_input())):
+        __operate_list(mylist, raw_input())
+
+
+def __operate_list(data_list, strparam):
+    if not strparam:
+        return
+    if 'print' == strparam:
+        print data_list
+    elif strparam.startswith('insert'):
+        params = strparam.split(' ', 3)
+        data_list.insert(int(params[1]), int(params[2]))
+    elif strparam.startswith('remove'):
+        params = strparam.split(' ', 2)
+        data_list.remove(int(params[1]))
+    elif strparam.startswith('append'):
+        params = strparam.split(' ', 2)
+        data_list.append(int(params[1]))
+    elif strparam.startswith('sort'):
+        data_list.sort()
+    elif strparam.startswith('pop'):
+        data_list.pop()
+    elif strparam.startswith('reverse'):
+        data_list.reverse()
 
 
 
